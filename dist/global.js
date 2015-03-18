be02e974718b5e03545fda9e607b3482c7bf1c71
Gary Katsevman <git@gkatsev.com>
2015-03-18 12:00:04 -0400

    New build

--- dist/global.js
+++ dist/global.js
@@ -1,33 +1,22 @@
 (function(f){var g;if(typeof window!=='undefined'){g=window}else if(typeof self!=='undefined'){g=self}g.videojsPlaylist=f()})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var resetadvance;
+
 module.exports = function autoadvance(player, timeout) {
-  var resetadvance = function resetadvance() {
-    if (player.playlist._timeoutId) {
-      window.clearTimeout(player.playlist._timeoutId);
-    }
-
-    if (player.playlist._ontimeout) {
-      player.off('ended', player.playlist._ontimeout);
-    }
-
-    player.playlist._timeoutId = null;
-    player.playlist._ontimeout = null;
-  };
-
   // we want to cancel the auto advance or auto advance was called with a bogus value
   if (typeof timeout !== 'number' || timeout !== timeout || timeout < 0 || timeout === Infinity) {
-    return resetadvance();
+    return resetadvance(player);
   }
 
   var ontimeout = function() {
     player.playlist._timeoutId = window.setTimeout(function() {
-      resetadvance();
+      resetadvance(player);
       player.playlist.next();
-    }, timeout);
+    }, timeout * 1000);
   };
 
   // we called auto advance while an auto-advance was in progress
   if (player.playlist._timeoutId) {
-    return resetadvance();
+    return resetadvance(player);
   }
 
   // we are starting a new video and don't have a timeout handler for it
@@ -37,9 +26,22 @@ module.exports = function autoadvance(player, timeout) {
   }
 
   // we want to reset the timeout for auto advance
-  resetadvance();
+  resetadvance(player);
   player.playlist._ontimeout = ontimeout;
   player.one('ended', ontimeout);
+};
+
+module.exports.resetadvance = resetadvance = function resetadvance(player) {
+  if (player.playlist._timeoutId) {
+    window.clearTimeout(player.playlist._timeoutId);
+  }
+
+  if (player.playlist._ontimeout) {
+    player.off('ended', player.playlist._ontimeout);
+  }
+
+  player.playlist._timeoutId = null;
+  player.playlist._ontimeout = null;
 };
 
 },{}],2:[function(require,module,exports){
@@ -87,6 +89,24 @@ var setupAutoadvance = require('./autoadvance.js');
 var isArray = Array.isArray || function(array) {
   return Object.prototype.toString.call(array) === '[object Array]';
 };
+var isInSources = function(arr, src) {
+  var i = 0;
+  var j = 0;
+  var item;
+  var source;
+
+  for (; i < arr.length; i++) {
+    item = arr[i];
+    for (; j < item.sources.length; j++) {
+      source = item.sources[j];
+      if (source && (source === src || source.src === src)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
 
 // factory method to return a new playlist with the following API
 // playlist(["a", "b", "c"]) // setter, ["a", "b", "c"]
@@ -108,13 +128,20 @@ var playlistMaker = function(player, plist) {
     if (plist && isArray(plist)) {
       list = plist.slice();
       player.playlist.currentItem(0);
+
+      window.setTimeout(function() {
+        player.trigger('playlistchange');
+      }, 0);
     }
 
     return list.slice();
   };
 
   playlist.currentItem = function item(index) {
-    if (typeof index === 'number' && index >= 0 && index < list.length) {
+    if (typeof index === 'number' &&
+        currentIndex !== index &&
+        index >= 0 &&
+        index < list.length) {
       currentIndex = index;
       playItem(player, autoadvanceTimeout, list[currentIndex]);
       return currentIndex;
@@ -154,6 +181,14 @@ var playlistMaker = function(player, plist) {
   if (list.length) {
     playlist.currentItem(0);
   }
+
+  player.on('loadstart', function() {
+    var currentSrc = player.currentSrc();
+    if (!isInSources(list, currentSrc)) {
+      currentIndex = -1;
+      setupAutoadvance.resetadvance(player);
+    }
+  });
 
   return playlist;
 };
